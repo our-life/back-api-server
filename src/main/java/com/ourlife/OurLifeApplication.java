268cf710855275251aba7ea6fe9d8b349d998597
kimjungmin <jungmini0601@gmail.com>
2022-12-18 09:28:49 +0900

    프로젝트 초기 세팅

---
+++ src/main/java/com/ourlife/OurLifeApplication.java
@@ -0,0 +1,13 @@
+package com.ourlife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurLifeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OurLifeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ourlife/OurLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ourlife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurLifeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
